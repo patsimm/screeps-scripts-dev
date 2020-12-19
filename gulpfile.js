6f6ed24345215b38326e2338ef393685c2736505
Patrick Simmelbauer <patrick.simmelbauer@gmail.com>
2020-12-19 20:36:56 +0100

    use gulp to build and commit

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const { src, dest, series } = require("gulp")
+var ts = require("gulp-typescript")
+var screeps = require("gulp-screeps")
+var clean = require("gulp-clean")
+require("dotenv").config()
+
+const buildTask = () => src("src/*.ts").pipe(ts()).pipe(dest("dist"))
+
+const cleanTask = () => src("dist", { allowEmpty: true }).pipe(clean())
+
+const screepsTask = () =>
+  src("dist/*.js").pipe(
+    screeps({
+      token: process.env.SCREEPS_API_KEY,
+      branch: "default",
+    })
+  )
+
+exports.default = series(cleanTask, buildTask)
+exports.screeps = series(cleanTask, buildTask, screepsTask)
+exports.clean = cleanTask
